d77457bfadc77c2f4070fb7a493efb3318b8ae30
LuisCastroV <>
2022-03-18 15:17:03 +0000

    new try

--- list.c
+++ list.c
@@ -45,7 +45,7 @@ void * firstList(List * list) {
 }
 
 void * nextList(List * list) {
-  if(!list->head || !list->tail || !list->current->next){
+  if(!list->head || !list->tail ||  !list->current || !list->current->next){
     return NULL;
   }
   list->current=list->current->next;
