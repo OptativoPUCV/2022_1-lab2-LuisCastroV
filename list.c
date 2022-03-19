2c434fa32a5af12f907dfb6afc989deee7dda465
LuisCastroV <>
2022-03-19 00:14:51 +0000

    new try

--- list.c
+++ list.c
@@ -69,6 +69,14 @@ void * prevList(List * list) {
 }
 
 void pushFront(List * list, void * data) {
+  Node* n = createNode(data);
+   n->next = list->head;
+   if(list->head){
+     list->head->prev = n;
+   }
+   list->head = n;
+   n->prev=NULL;
+   //list->size++;
 }
 
 void pushBack(List * list, void * data) {
