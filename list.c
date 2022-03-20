09f04fb3d658f87e4282ecc0008f059f6140a39d
LuisCastroV <>
2022-03-20 20:26:27 +0000

    new try

--- list.c
+++ list.c
@@ -123,11 +123,10 @@ void * popCurrent(List * list){
   Node* nodoeliminado=list->current;
   void* data=list->current->data;
   
-  if(list->head==NULL && list->tail==NULL)
-  {
+  if(list->head != list->current && list->tail != list->current ){
     list->current->next->prev=list->current->prev;
     list->current->prev->next=list->current->next;
-    list->current=list->current->next;  
+    list->current=list->current->next; 
   }
   else{
     if(list->current==list->head)
